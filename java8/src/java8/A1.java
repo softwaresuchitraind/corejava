7c7e387420a945eb53a2ba92e90722f5e708d055
softwaresuchitraind <software.suchitra@gmail.com>
2023-08-20 21:03:12 +0530

    test3

--- java8/src/java8/A1.java
+++ java8/src/java8/A1.java
@@ -1,6 +1,7 @@
 public class A1
 {
     public static void main(String[] args) {
+        System.out.println("Bangalore Welcome. Mphasis..!!");
         System.out.println("Bangalore Welcome.Wipro..!!");
     }
 }
